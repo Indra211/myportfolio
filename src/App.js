6d044e28dbd390c26392598f2e7c45081db975a5
Indra211 <indrasena0018@gmail.com>
2024-03-16 12:56:27 +0530

    loader added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Home from "./Container/Home/Home";
 import About from "./Container/About/About";
@@ -7,37 +7,56 @@ import ContactMe from "./Container/ContactMe/ContactMe";
 import Navbar from "./Components/navbar/navbar";
 import { ToastContainer } from "react-toastify";
 import ScrollAnimation from "react-animate-on-scroll";
+import { Loader } from "./Components/Loader/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 2000);
+  }, []);
+
   return (
-    <div className="App">
-      <div className="background-image"></div>
-      <div className="overlay"></div>
-      <Navbar />
-      <div className="content">
-        <section id="home">
-          <ScrollAnimation animateIn="fadeIn">
-            <Home />
-          </ScrollAnimation>
-        </section>
-        <section id="about">
-          <ScrollAnimation animateIn="bounceInLeft">
-            <About />
-          </ScrollAnimation>
-        </section>
-        <section id="resume">
-          <ScrollAnimation animateIn="bounceInRight">
-            <ResumeInFo />
-          </ScrollAnimation>
-        </section>
-        <section id="contact">
-          <ScrollAnimation animateIn="flipInY">
-            <ContactMe />
-          </ScrollAnimation>
-        </section>
-      </div>
-      <ToastContainer />
-    </div>
+    <>
+      {!loading ? (
+        <div className="loading">
+          <Loader />
+        </div>
+      ) : (
+        <ScrollAnimation animateIn="fadeIn">
+          <div className="App">
+            <div className="background-image"></div>
+            <div className="overlay"></div>
+            <Navbar />
+            <div className="content">
+              <section id="home">
+                <ScrollAnimation animateIn="fadeIn">
+                  <Home />
+                </ScrollAnimation>
+              </section>
+              <section id="about">
+                <ScrollAnimation animateIn="bounceInLeft">
+                  <About />
+                </ScrollAnimation>
+              </section>
+              <section id="resume">
+                <ScrollAnimation animateIn="bounceInRight">
+                  <ResumeInFo />
+                </ScrollAnimation>
+              </section>
+              <section id="contact">
+                <ScrollAnimation animateIn="flipInY">
+                  <ContactMe />
+                </ScrollAnimation>
+              </section>
+            </div>
+            <ToastContainer />
+          </div>
+        </ScrollAnimation>
+      )}
+    </>
   );
 }
 
---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Loader.css";
+
+export const Loader = () => {
+  return (
+    <div className="container">
+      <div className="loader-1">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <p>Stay Tuned....</p>
+      </div>
+    </div>
+  );
+};
